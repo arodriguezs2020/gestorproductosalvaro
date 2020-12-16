6ce0c0d944ff5027b22a7685e8092e9de7f5f303
Alvaro Rodriguez <dam16.2020@gmail.com>
2020-12-16 13:35:10 +0100

    Commit

--- src/main/java/org/jesuitasrioja/holamundo/controllers/ProductoController.java
+++ src/main/java/org/jesuitasrioja/holamundo/controllers/ProductoController.java
@@ -1,56 +1,61 @@
 package org.jesuitasrioja.holamundo.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jesuitasrioja.holamundo.modelo.Producto;
+import org.jesuitasrioja.holamundo.repository.ProductosRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductoController {
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+public class ProductoController implements IProductosRepository {
 
+	private ProductosRepository pr = new ProductosRepository();
+
+	@Override
 	@GetMapping("/productos")
 	public List<Producto> allProducts() {
 
-		List<Producto> listaProductos = new ArrayList<Producto>();
-
-		listaProductos.add(new Producto("1", "Tomate", 1.30));
-		listaProductos.add(new Producto("2", "Patata", 0.50));
-		listaProductos.add(new Producto("3", "Aguacate", 3.0));
-
-		return listaProductos;
+		return pr.getAll();
 	}
 
+	@Override
 	@GetMapping("/producto/{id}")
 	public Producto getProducto(@PathVariable String id) {
-		return new Producto(id, "Tomate", 1.30);
+		return pr.getById(id);
 	}
 
+	@Override
 	@GetMapping("/producto")
 	public Producto getProducto2(@RequestParam String id) {
-		return new Producto(id, "Tomate", 1.30);
+		return pr.getById(id);
 	}
 
+	@Override
 	@PostMapping("/producto")
 	public String postProducto(@RequestBody Producto nuevoProducto) {
-		return "Añadido: " + nuevoProducto.toString();
+		return String.valueOf(pr.aniadirProducto(nuevoProducto));
 	}
 
+	@Override
 	@PutMapping("/producto")
 	public String putProducto(@RequestBody Producto editadoProducto) {
 		return null;
 	}
 
+	@Override
 	@DeleteMapping("/producto/{id}")
 	public String deleteProducto(@PathVariable String id) {
-		return null;
+		return String.valueOf(pr.eliminarProducto(id));
 	}
 
 //	@GetMapping("/saludos/{nombre}")
